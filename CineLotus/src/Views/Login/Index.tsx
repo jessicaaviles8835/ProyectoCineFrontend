270043d4f8602410683d56cc11d7431dbc5b0a12
jessicaaviles8835 <45468048+jessicaaviles8835@users.noreply.github.com>
2025-03-20 14:25:02 -0500

    Login form
    Agregando la funcionalidad para hacer login en el backend

--- CineLotus/src/Views/Login/Index.tsx
+++ CineLotus/src/Views/Login/Index.tsx
@@ -2,10 +2,50 @@ import { Avatar, Box, Button, Paper, TextField } from "@mui/material"
 import IconoRojo from "../../../public/IconoUserRojo.png"
 import { AccountCircle } from "@mui/icons-material"
 import HttpsIcon from '@mui/icons-material/Https';
+import React, { useState } from 'react';
+import axios from 'axios';
+
 
 
 
 export const Login: React.FC<{}> = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [token, setToken] = useState<string | null>(null);
+
+    const handleLogin = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+          const response = await axios.post('http://localhost:3000/users/login', {
+            username,
+            password
+          });
+          setToken(response.data.token);
+          localStorage.setItem('token', response.data.token);
+        } catch (error) {
+          console.error('Error al iniciar sesión:', error);
+        }
+      };
+      
+      const handleProfile = async () => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+          console.log('No estás autenticado');
+          return;
+        }
+    
+        try {
+          const response = await axios.get('http://localhost:3000/users/profile', {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          });
+          console.log(response.data.message);
+        } catch (error) {
+          console.error('Error al obtener perfil:', error);
+        }
+      };
+
     return (
         <Box sx={{ bgcolor: "#d8d8d8", width: '100%', height: '100vh', display: "flex", justifyContent: "center", alignItems: "center" }}>
             <Paper elevation={24} sx={{ width: 400, height: 300, borderRadius: "10%" }}>
@@ -15,16 +55,19 @@ export const Login: React.FC<{}> = () => {
                 <Box sx={{display:"grid", placeItems:"center", rowGap:2}}>
                     <Box sx={{ display: 'flex', alignItems: 'flex-end',  width:"80%" }}>
                         <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                        <TextField id="input-user-name" label="Nombre de usuario" variant="standard" sx={{width:"100%"}}/>
+                        <TextField id="input-user-name" label="Nombre de usuario" variant="standard" sx={{width:"100%"}} onChange={(e) => setUsername(e.target.value)}/>
                     </Box>
                     <Box sx={{ display: 'flex', alignItems: 'flex-end', width:"80%" }}>
                         <HttpsIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                        <TextField id="input-password" label="Contraseña" variant="standard" sx={{width:"100%"}}/>
+                        <TextField id="input-password" label="Contraseña" variant="standard" sx={{width:"100%"}} onChange={(e) => setPassword(e.target.value)}/>
                     </Box>
                     <Box sx={{marginTop:3}}>
-                        <Button sx={{bgcolor:"#ff6b6b"}} variant="contained">Iniciar sesión</Button>
+                        <Button sx={{bgcolor:"#ff6b6b"}} variant="contained" onClick={handleLogin}>Iniciar sesión</Button>
                     </Box>
                 </Box>
+                <button onClick={handleProfile}>Ver perfil</button>
+
+                {token && <div>Token JWT: {token}</div>}
             </Paper>
         </Box>
     )
